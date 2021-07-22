f8d8d9e40b59a1f7c7963a740503f64abfdde1d1
Padraic Calpin <padraic.calpin93@gmail.com>
2021-07-22 10:22:29 +0200

    Test the deploy keys

--- bitbucket/resource_deploy_key.go
+++ bitbucket/resource_deploy_key.go
@@ -5,22 +5,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"strings"
 )
 
 type DeployKey struct {
-	Label   string `json:"label,omitempty`
-	Key     string `json:key`
-	Comment string `json:comment,omitempty`
-	Id      string `json:id,omitempty`
+	Label   string `json:"label,omitempty"`
+	Key     string `json:"key,omitempty"`
+	Comment string `json:"comment,omitempty"`
+	Id      int `json:"id,omitempty"`
 }
 
 func resourceDeployKey() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceDeployKeyCreate,
-		Update: resourceDeployKeyUpdate,
 		Read:   resourceDeployKeyRead,
 		Delete: resourceDeployKeyDelete,
 
@@ -29,6 +29,12 @@ func resourceDeployKey() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					if strings.TrimSpace(old) == strings.TrimSpace(new) {
+						return true
+					}
+					return false
+				},
 			},
 			"repository": {
 				Type:     schema.TypeString,
@@ -38,10 +44,12 @@ func resourceDeployKey() *schema.Resource {
 			"name": {
 				Type:     schema.TypeString,
 				Optional: true,
+				ForceNew: true,
 			},
 			"comment": {
 				Type:     schema.TypeString,
 				Optional: true,
+				ForceNew: true,
 			},
 		},
 	}
@@ -49,7 +57,7 @@ func resourceDeployKey() *schema.Resource {
 
 func newDeployKeyFromResource(d *schema.ResourceData) *DeployKey {
 	deployKey := &DeployKey{
-		Key:     d.Get("public_key").(string),
+		Key:     strings.TrimSpace(d.Get("public_key").(string)),
 		Label:   d.Get("name").(string),
 		Comment: d.Get("comment").(string),
 	}
@@ -59,18 +67,13 @@ func newDeployKeyFromResource(d *schema.ResourceData) *DeployKey {
 
 func resourceDeployKeyCreate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
-	deployKey = newDeployKeyFromResource(d)
+	deployKey := newDeployKeyFromResource(d)
 
 	bytedata, err := json.Marshal(deployKey)
 
 	if err != nil {
 		return err
 	}
-
-	repo_parts := strings.Split(d.Get("repository").(string))
-	var owner, repo_slug string
-	owner = repo_parts[0]
-	repo_slug = repo_parts[1]
 
 	deployKeyResponse, posterr := client.Post(fmt.Sprintf("2.0/repositories/%s/deploy-keys",
 		d.Get("repository").(string),
@@ -79,8 +82,6 @@ func resourceDeployKeyCreate(d *schema.ResourceData, m interface{}) error {
 	if posterr != nil {
 		return posterr
 	}
-
-	var deployKey DeployKey
 
 	body, readerr := ioutil.ReadAll(deployKeyResponse.Body)
 	if readerr != nil {
@@ -92,25 +93,30 @@ func resourceDeployKeyCreate(d *schema.ResourceData, m interface{}) error {
 		return decodeerr
 	}
 
-	d.SetId(string(fmt.Sprintf("%s:%s", d.Get("repository").(string), deployKey.Id)))
+	d.SetId(string(fmt.Sprintf("%s:%d", d.Get("repository").(string), deployKey.Id)))
 
 	return resourceDeployKeyRead(d, m)
 }
 
-func resourceDeployKeyRead(d *schema.Resource, m interface{}) error {
+func resourceDeployKeyRead(d *schema.ResourceData, m interface{}) error {
 	id := d.Id()
+	var keyId int
+	var iderr error
 	if id != "" {
 		idparts := strings.Split(id, ":")
 		if len(idparts) == 2 {
 			d.Set("repository", idparts[0])
-			keyId := idparts[1]
+			keyId, iderr = strconv.Atoi(idparts[1])
+			if iderr != nil {
+				return iderr
+			}
 		} else {
 			return fmt.Errorf("Incorrect ID format, should match `owner/repo_name:key_id`")
 		}
 	}
 
 	client := m.(*Client)
-	deployKeyReq, err := client.Get(fmt.Sprintf("2.0/repositories/%s/deploy-keys/%s",
+	deployKeyReq, err := client.Get(fmt.Sprintf("2.0/repositories/%s/deploy-keys/%d",
 		d.Get("repository").(string),
 		keyId,
 	))
@@ -143,49 +149,22 @@ func resourceDeployKeyRead(d *schema.Resource, m interface{}) error {
 
 }
 
-func resourceDeployKeyUpdate(d *schema.Resource, m interface{}) error {
-	client := m.(*Client)
-	deployKey := newDeployKeyFromResource(d)
-	// Set key to empty, as only label and comment can be changed via PUT
-	deployKey.Key = ""
-
-	var jsonbuffer []byte
-	payload := bytes.NewBuffger(jsonbuffer)
-	enc := json.NewEncoder(jsonpayload)
-	enc.Encode(deployKey)
-
-	var keyId string
-	idparts := strings.Split(d.Id(), ":")
-	if len(idparts) == 2 {
-		keyId = idparts[1]
-	} else {
-		return fmt.Errorf("Incorrect ID format, should match `owner/repo_name:key_id`")
-	}
-
-	_, err := client.Put(fmt.Sprintf("2.0/repositories/%s/deploy-keys/%s",
-		d.Get("repository").(string),
-		keyId,
-	), payload)
-
-	if err != nil {
-		return err
-	}
-	return resourceProjectRead(d, m)
-
-}
-
-func resourceDeployKeyDelete(d *schema.Resource, m interface{}) error {
-	var keyId string
+func resourceDeployKeyDelete(d *schema.ResourceData, m interface{}) error {
+	var keyId int
+	var iderr error
 
 	idparts := strings.Split(d.Id(), ":")
 	if len(idparts) == 2 {
-		keyId = idparts[1]
+		keyId, iderr = strconv.Atoi(idparts[1])
+		if iderr != nil {
+			return iderr
+		}
 	} else {
 		return fmt.Errorf("Incorrect ID format, should match `owner/repo_name:key_id`")
 	}
 
 	client := m.(*Client)
-	_, err := client.Delete(fmt.Sprintf("2.0/repositories/%s/deploy-keys/%s",
+	_, err := client.Delete(fmt.Sprintf("2.0/repositories/%s/deploy-keys/%d",
 		d.Get("repository").(string),
 		keyId,
 	))
